2969e49089bfb830aa5c59a902d820b7731ffdad
Robert Newton <robert.newton11@gmail.com>
2025-04-12 08:46:11 -0700

    initialize with vite + react

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="placeholder">
+      <img src="/public/quetzal-construction.jpg" alt="Quetzal with a Hard Hat" />
+      <div>
+        <h1>Bird with Aadu is under construction. Check back soon!</h1>
+        <p>
+          Original image by Flickr user
+          <a
+            rel="nofollow"
+            href="https://www.flickr.com/photos/chdwckvnstrsslhm/"
+          >
+            chdwckvnstrsslhm
+          </a>
+          . Photo uploaded to commons by user
+          <a
+            href="//commons.wikimedia.org/wiki/User:Ltshears"
+            title="User:Ltshears"
+          >
+            ltshears
+          </a>
+          - Flickr
+          <a
+            rel="nofollow"
+            href="https://www.flickr.com/photos/chdwckvnstrsslhm/128203201/"
+          >
+            here
+          </a>
+          ,
+          <a
+            href="https://creativecommons.org/licenses/by/2.0"
+            title="Creative Commons Attribution 2.0"
+          >
+            CC BY 2.0
+          </a>
+          ,
+          <a href="https://commons.wikimedia.org/w/index.php?curid=1833937">
+            Link
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
