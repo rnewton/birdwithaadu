934a15e73042cea332591900a3cee838b255e1f0
Robert Newton <robert.newton11@gmail.com>
2025-04-12 10:01:57 -0700

    fix issue with missing markers

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import L from 'leaflet';
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+import "leaflet/dist/leaflet.css";
 import "./App.css";
+
+// Fix marker icon issue
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+const DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 function App() {
   return (
