92ecbce1de1dbe3a9dd44130ff92188f4b083ba8
Robert Newton <robert.newton11@gmail.com>
2025-04-12 10:12:43 -0700

    fix styles

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,11 @@ import icon from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 
 const DefaultIcon = L.icon({
-    iconUrl: icon,
-    shadowUrl: iconShadow
+  iconSize: [25, 41],
+  iconAnchor: [10, 41],
+  popupAnchor: [2, -40],
+  iconUrl: icon,
+  shadowUrl: iconShadow,
 });
 
 L.Marker.prototype.options.icon = DefaultIcon;
