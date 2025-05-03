5c77978928d034ac52a3dc30013f408c9c0344c7
Robert Newton <robert.newton11@gmail.com>
2025-05-03 09:45:45 -0700

    copy points when scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -69,7 +69,7 @@ function App() {
   console.log(data);
 
   return (
-    <MapContainer className="map-container" center={[47.6, -122.33]} zoom={11}>
+    <MapContainer className="map-container" center={[47.6, -122.33]} zoom={11} worldCopyJump>
       <TileLayer
         attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
